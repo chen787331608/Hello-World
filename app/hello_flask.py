c04137d1170427850fe94e74379c0f90d88beee5
chen787331608 <chen787331608@163.com>
2016-12-06 09:54:07 +0800

    update app.py

--- app/hello_flask.py
+++ app/hello_flask.py
@@ -3,8 +3,8 @@
 # Filename:hello_flask.py
 
 from flask import Flask, render_template, session, redirect, url_for
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.wtf import Form
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 import sys
--- app/hello_flask_9.py
+++ app/hello_flask_9.py
@@ -4,4 +4,4 @@
 import hello_flask
 
 if __name__ == '__main__':
-    hello_flask.app.run(host="0.0.0.0", port=8889, debug=True)
+    hello_flask.app.run(host="0.0.0.0", port=80, debug=True)
